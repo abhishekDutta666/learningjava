caf77b820778a3885eccdfd4f08eb04243b4e1ac
AbhishekDutta666 <mailabhishekdutta@gmail.com>
2019-02-13 02:09:00 +0530

    adding first log4j class

---
+++ learningjava/src/com/abhishek/FirstLog4j.java
@@ -0,0 +1,31 @@
+package com.abhishek;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class FirstLog4j {
+
+	static{
+		init();
+	}
+	
+	private final static Logger logger = Logger.getLogger(FirstLog4j.class);
+	
+	public static void main(String[] args) {
+
+		logger.debug("My Debug Log");
+		logger.info("My Info Log");
+		logger.warn("My Warn Log");
+		logger.error("My error log");
+		logger.fatal("My fatal log");
+				
+	}
+
+	/**
+	 * method to init log4j configurations
+	 */
+	private static void init() {
+		PropertyConfigurator.configure("log4j.properties");
+	}
+
+}
